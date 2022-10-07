bc68fcf465d896dac1bac088eb2050b403f4292f
Anto Subash <antosubash@live.com>
2022-10-07 11:48:23 +0200

    fix: updated the api and id urls

--- GeoWiki.Cli/Infrastructure/Constants.cs
+++ GeoWiki.Cli/Infrastructure/Constants.cs
@@ -2,11 +2,14 @@ namespace GeoWiki.Cli.Infrastructure
 {
     public static class Constants
     {
+
+#if DEBUG
         public const string ApiUrl = "https://localhost:44390";
         public const string IdentityUrl = "https://localhost:44386";
-#if DEBUG
         public const bool IgnoreSslErrors = true;
 #else
+        public const string ApiUrl = "https://api.v2.geo-wiki.org";
+        public const string IdentityUrl = "https://id.v2.geo-wiki.org";
         public const bool IgnoreSslErrors = false;
 #endif
     }
